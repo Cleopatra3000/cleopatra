b39751ceeede02777bd8e733f0d543b434a0d560
Cleopatra <cleopatra_personal3000@hotmail.com>
2026-01-15 21:52:41 +0100

    footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,13 +1,44 @@
 import styles from './footer.module.css'
-import FooterNavTabs from './components/FooterNavTabs/FooterNavTabs'
+import whasapimage from '../../images/whasapp.png'
+import escribirImagen from '../../images/escribeme.png'
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.inner}>
-        <div>Footer</div>
-        <FooterNavTabs />
-      </div>
+
+          <div className={styles.inner1}>
+
+                <img
+                  src={escribirImagen}
+                  alt="Escríbeme"
+                  className={styles.escribirImagen}
+                />
+
+                {/* BOTÓN WHATSAPP */}
+                <a
+                  href="https://wa.me/34603257735?text=hola"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.whatsappLink}
+                >
+                      <div className={styles.inner2}>
+
+                            <img
+                              src={whasapimage}
+                              alt="WhatsApp"
+                              className={styles.whasapimage}
+                            />
+
+                            <div className={styles.numeroTelefono}>
+                              603257735
+                            </div>
+
+                      </div>
+                      
+                </a>
+
+          </div>
+
     </footer>
   )
 }
