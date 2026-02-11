ef2f89183e8965e7f4bc5f9678f10c4e58875ca1
Cleopatra <cleopatra_personal3000@hotmail.com>
2026-02-11 20:19:26 +0100

    fotos

--- src/pages/tab1/tab1.jsx
+++ src/pages/tab1/tab1.jsx
@@ -3,6 +3,9 @@ import foto1 from '../../images/fotos/foto1.jpg'
 import foto2 from '../../images/fotos/foto2.jpg'
 import foto3 from '../../images/fotos/foto3.jpg'
 import foto4 from '../../images/fotos/foto4.jpg'
+import foto5 from '../../images/fotos/foto5.jpg'
+import foto6 from '../../images/fotos/foto6.jpg'
+import foto7 from '../../images/fotos/foto7.jpg'
 import styles from './tab1.module.css'
 
 
@@ -19,6 +22,12 @@ const Tab1 = () => {
         <Card imageSrc={foto3} imageAlt="Foto" />
 
         <Card imageSrc={foto4} imageAlt="Foto" />
+
+        <Card imageSrc={foto5} imageAlt="Foto" />
+
+        <Card imageSrc={foto6} imageAlt="Foto" />
+
+        <Card imageSrc={foto7} imageAlt="Foto" />
       
     </section>
   )
