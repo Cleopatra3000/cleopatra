08a5f55ddc9af9723b4f5fdf94cd46db10300259
Cleopatra <cleopatra_personal3000@hotmail.com>
2026-02-12 20:44:23 +0100

    fotos

--- src/pages/tab1/tab1.jsx
+++ src/pages/tab1/tab1.jsx
@@ -2,7 +2,7 @@ import Card from "../../components/Card/card"
 import foto1 from '../../images/fotos/foto1.jpg'
 import foto2 from '../../images/fotos/foto2.jpg'
 import foto3 from '../../images/fotos/foto3.jpg'
-import foto4 from '../../images/fotos/foto4.jpg'
+// import foto4 from '../../images/fotos/foto4.jpg'
 import foto5 from '../../images/fotos/foto5.jpg'
 import foto6 from '../../images/fotos/foto6.jpg'
 import foto7 from '../../images/fotos/foto7.jpg'
@@ -21,7 +21,7 @@ const Tab1 = () => {
 
         <Card imageSrc={foto3} imageAlt="Foto" />
 
-        <Card imageSrc={foto4} imageAlt="Foto" />
+        {/* <Card imageSrc={foto4} imageAlt="Foto" /> */}
 
         <Card imageSrc={foto5} imageAlt="Foto" />
 
